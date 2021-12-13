4b0109f3558b4d63955d17b223824701d78ad6a2
V1ewsonic <77628663+V1ewsonic@users.noreply.github.com>
2021-12-13 13:04:08 +0700

    updated
    Delete library seaborn & matplotlib

--- app2.py
+++ app2.py
@@ -1,8 +1,6 @@
 import streamlit as st
 import numpy as np
 import pandas as pd
-import seaborn as sns
-import matplotlib.pyplot as plt
 # me-non aktifkan peringatan pada python
 import warnings
 warnings.filterwarnings('ignore')
